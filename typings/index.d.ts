1584618ca73a10cea95449da7e40db0b7a4cde8c
5ebastianMeier <sebastian.meier@5minds.de>
2017-01-25 14:04:55 +0100

    Initial commit

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/morgan/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/morgan/index.d.ts" />
