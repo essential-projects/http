9505a11d365918c1ad2c44d2a310d490bd12c169
Patrick Poetschke <patrick.poetschke@quantusflow.com>
2017-02-27 22:18:51 +0100

    fix missing dists

--- dist/commonjs/http_client.js
+++ dist/commonjs/http_client.js
@@ -4,77 +4,137 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+        step((generator = generator.apply(thisArg, _arguments)).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const popsicle = require("popsicle");
-class HttpClient {
-    constructor() {
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var popsicle = require("popsicle");
+var HttpClient = (function () {
+    function HttpClient() {
         this.config = undefined;
     }
-    _buildRequestOptions(method, url) {
-        const baseUrl = this.config.url ? `${this.config.url}/` : '';
+    HttpClient.prototype._buildRequestOptions = function (method, url) {
+        var baseUrl = this.config.url ? this.config.url + "/" : '';
         return {
             method: method,
-            url: `${baseUrl}${url}`,
+            url: "" + baseUrl + url,
             headers: {
                 'Content-Type': 'application/json'
             }
         };
-    }
-    get(url) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const requestOptions = this._buildRequestOptions('GET', url);
-            const result = yield popsicle.request(requestOptions);
-            if (result.status < 200 || result.status >= 300) {
-                throw new Error(result.body);
-            }
-            const response = {
-                result: result.body,
-                status: result.status
-            };
-            return response;
+    };
+    HttpClient.prototype.get = function (url) {
+        return __awaiter(this, void 0, void 0, function () {
+            var requestOptions, result, response;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        requestOptions = this._buildRequestOptions('GET', url);
+                        return [4 /*yield*/, popsicle.request(requestOptions)];
+                    case 1:
+                        result = _a.sent();
+                        if (result.status < 200 || result.status >= 300) {
+                            throw new Error(result.body);
+                        }
+                        response = {
+                            result: result.body,
+                            status: result.status
+                        };
+                        return [2 /*return*/, response];
+                }
+            });
         });
-    }
-    post(url, data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const requestOptions = this._buildRequestOptions('POST', url);
-            const result = yield popsicle.request(requestOptions);
-            if (result.status < 200 || result.status >= 300) {
-                throw new Error(result.body);
-            }
-            const response = {
-                result: result.body,
-                status: result.status
-            };
-            return response;
+    };
+    HttpClient.prototype.post = function (url, data) {
+        return __awaiter(this, void 0, void 0, function () {
+            var requestOptions, result, response;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        requestOptions = this._buildRequestOptions('POST', url);
+                        return [4 /*yield*/, popsicle.request(requestOptions)];
+                    case 1:
+                        result = _a.sent();
+                        if (result.status < 200 || result.status >= 300) {
+                            throw new Error(result.body);
+                        }
+                        response = {
+                            result: result.body,
+                            status: result.status
+                        };
+                        return [2 /*return*/, response];
+                }
+            });
         });
-    }
-    put(url, data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const requestOptions = this._buildRequestOptions('PUT', url);
-            const result = yield popsicle.request(requestOptions);
-            if (result.status < 200 || result.status >= 300) {
-                throw new Error(result.body);
-            }
-            const response = {
-                result: result.body,
-                status: result.status
-            };
-            return response;
+    };
+    HttpClient.prototype.put = function (url, data) {
+        return __awaiter(this, void 0, void 0, function () {
+            var requestOptions, result, response;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        requestOptions = this._buildRequestOptions('PUT', url);
+                        return [4 /*yield*/, popsicle.request(requestOptions)];
+                    case 1:
+                        result = _a.sent();
+                        if (result.status < 200 || result.status >= 300) {
+                            throw new Error(result.body);
+                        }
+                        response = {
+                            result: result.body,
+                            status: result.status
+                        };
+                        return [2 /*return*/, response];
+                }
+            });
         });
-    }
-    delete(url) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const requestOptions = this._buildRequestOptions('DELETE', url);
-            const result = yield popsicle.request(requestOptions);
-            if (result.status !== 200) {
-                throw new Error(result.body);
-            }
+    };
+    HttpClient.prototype.delete = function (url) {
+        return __awaiter(this, void 0, void 0, function () {
+            var requestOptions, result;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        requestOptions = this._buildRequestOptions('DELETE', url);
+                        return [4 /*yield*/, popsicle.request(requestOptions)];
+                    case 1:
+                        result = _a.sent();
+                        if (result.status !== 200) {
+                            throw new Error(result.body);
+                        }
+                        return [2 /*return*/];
+                }
+            });
         });
-    }
-}
+    };
+    return HttpClient;
+}());
 exports.HttpClient = HttpClient;
 
 //# sourceMappingURL=http_client.js.map
--- dist/commonjs/http_extension.js
+++ dist/commonjs/http_extension.js
@@ -1,12 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Express = require("express");
-const BluebirdPromise = require("bluebird");
-const utils_1 = require("@process-engine-js/utils");
-const BodyParser = require("body-parser");
-const core_contracts_1 = require("@process-engine-js/core_contracts");
-class HttpExtension {
-    constructor(container) {
+var Express = require("express");
+var BluebirdPromise = require("bluebird");
+var utils_1 = require("@process-engine-js/utils");
+var BodyParser = require("body-parser");
+var core_contracts_1 = require("@process-engine-js/core_contracts");
+var HttpExtension = (function () {
+    function HttpExtension(container) {
         this._container = undefined;
         this._routers = {};
         this._app = undefined;
@@ -14,40 +13,58 @@ class HttpExtension {
         this.config = undefined;
         this._container = container;
     }
-    get routers() {
-        return this._routers;
-    }
-    get container() {
-        return this._container;
-    }
-    get app() {
-        if (!this._app) {
-            this._app = Express();
-        }
-        return this._app;
-    }
-    get server() {
-        return this._server;
-    }
-    initialize() {
+    Object.defineProperty(HttpExtension.prototype, "routers", {
+        get: function () {
+            return this._routers;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(HttpExtension.prototype, "container", {
+        get: function () {
+            return this._container;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(HttpExtension.prototype, "app", {
+        get: function () {
+            if (!this._app) {
+                this._app = Express();
+            }
+            return this._app;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(HttpExtension.prototype, "server", {
+        get: function () {
+            return this._server;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    HttpExtension.prototype.initialize = function () {
+        var _this = this;
         return utils_1.executeAsExtensionHookAsync(this.initializeAppExtensions, this, this.app)
-            .then(() => {
-            this.initializeBaseMiddleware(this.app);
-            return utils_1.executeAsExtensionHookAsync(this.initializeMiddlewareBeforeRouters, this, this.app);
+            .then(function () {
+            _this.initializeBaseMiddleware(_this.app);
+            return utils_1.executeAsExtensionHookAsync(_this.initializeMiddlewareBeforeRouters, _this, _this.app);
         })
-            .then(() => {
-            return this.initializeRouters();
+            .then(function () {
+            return _this.initializeRouters();
         })
-            .then(() => {
-            return utils_1.executeAsExtensionHookAsync(this.initializeMiddlewareAfterRouters, this, this.app);
+            .then(function () {
+            return utils_1.executeAsExtensionHookAsync(_this.initializeMiddlewareAfterRouters, _this, _this.app);
         });
-    }
-    initializeRouters() {
-        let routerNames;
-        const allRouterNames = this.container.getKeysByTags(core_contracts_1.RouterDiscoveryTag);
+    };
+    HttpExtension.prototype.initializeRouters = function () {
+        var _this = this;
+        var routerNames;
+        var allRouterNames = this.container.getKeysByTags(core_contracts_1.RouterDiscoveryTag);
         this.container.validateDependencies();
         return utils_1.executeAsExtensionHookAsync(this.filterRouters, this, allRouterNames)
-            .then((filteredRouterNames) => {
+            .then(function (filteredRouterNames) {
             if (typeof filteredRouterNames === 'undefined' || filteredRouterNames == null) {
                 routerNames = allRouterNames;
             }
@@ -60,61 +77,64 @@ class HttpExtension {
                 routerNames = filteredRouterNames;
             }
         })
-            .then(() => {
-            const serialPromise = routerNames.reduce((prevPromise, routerName) => {
-                return prevPromise.then(() => {
-                    return this.initializeRouter(routerName);
+            .then(function () {
+            var serialPromise = routerNames.reduce(function (prevPromise, routerName) {
+                return prevPromise.then(function () {
+                    return _this.initializeRouter(routerName);
                 });
             }, BluebirdPromise.resolve());
             return serialPromise;
         });
-    }
-    initializeRouter(routerName) {
+    };
+    HttpExtension.prototype.initializeRouter = function (routerName) {
+        var _this = this;
         if (!this.container.isRegistered(routerName)) {
-            throw new Error(`There is no router registered for key '${routerName}'`);
+            throw new Error("There is no router registered for key '" + routerName + "'");
         }
-        const routerInstance = this.container.resolve(routerName);
+        var routerInstance = this.container.resolve(routerName);
         return utils_1.executeAsExtensionHookAsync(routerInstance.initialize, routerInstance)
-            .then(() => {
-            this.bindRoute(routerInstance);
-            this.routers[routerName] = routerInstance;
+            .then(function () {
+            _this.bindRoute(routerInstance);
+            _this.routers[routerName] = routerInstance;
         });
-    }
-    bindRoute(routerInstance) {
-        const shieldingRouter = Express.Router();
-        shieldingRouter.use(`/${routerInstance.baseRoute}/`, routerInstance.router);
+    };
+    HttpExtension.prototype.bindRoute = function (routerInstance) {
+        var shieldingRouter = Express.Router();
+        shieldingRouter.use("/" + routerInstance.baseRoute + "/", routerInstance.router);
         this.app.use('/', shieldingRouter);
-    }
-    start() {
-        return new BluebirdPromise((resolve, reject) => {
-            this._server = this.app.listen(this.config.server.port, this.config.server.host, () => {
-                console.log(`Started REST API ${this.config.server.host}:${this.config.server.port}`);
-                utils_1.executeAsExtensionHookAsync(this.onStarted, this)
-                    .then((result) => {
+    };
+    HttpExtension.prototype.start = function () {
+        var _this = this;
+        return new BluebirdPromise(function (resolve, reject) {
+            _this._server = _this.app.listen(_this.config.server.port, _this.config.server.host, function () {
+                console.log("Started REST API " + _this.config.server.host + ":" + _this.config.server.port);
+                utils_1.executeAsExtensionHookAsync(_this.onStarted, _this)
+                    .then(function (result) {
                     resolve(result);
                 })
-                    .catch((error) => {
+                    .catch(function (error) {
                     reject(error);
                 });
             });
         });
-    }
-    close() {
+    };
+    HttpExtension.prototype.close = function () {
         if (this.server) {
             this.server.close();
         }
-    }
-    initializeAppExtensions(app) { }
-    initializeMiddlewareBeforeRouters(app) { }
-    initializeMiddlewareAfterRouters(app) { }
-    filterRouters(routerNames) {
+    };
+    HttpExtension.prototype.initializeAppExtensions = function (app) { };
+    HttpExtension.prototype.initializeMiddlewareBeforeRouters = function (app) { };
+    HttpExtension.prototype.initializeMiddlewareAfterRouters = function (app) { };
+    HttpExtension.prototype.filterRouters = function (routerNames) {
         return routerNames;
-    }
-    onStarted() { }
-    initializeBaseMiddleware(app) {
+    };
+    HttpExtension.prototype.onStarted = function () { };
+    HttpExtension.prototype.initializeBaseMiddleware = function (app) {
         app.use(BodyParser.json());
-    }
-}
+    };
+    return HttpExtension;
+}());
 exports.HttpExtension = HttpExtension;
 
 //# sourceMappingURL=http_extension.js.map
