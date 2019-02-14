2db39ae231d9e60231ef908f6886edbeeeab995e
ElRaptorus <christian.werner@5minds.de>
2019-02-14 12:20:56 +0100

    :sparkles: Expect config at http:http_client

--- ioc_module.js
+++ ioc_module.js
@@ -5,6 +5,7 @@ const HttpClient = require('./dist/commonjs/index').HttpClient;
 function registerInContainer(container) {
 
   container.register('HttpClient', HttpClient)
+    .configure('http:http_client')
     .singleton();
 
 }
